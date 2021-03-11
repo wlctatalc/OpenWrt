79501b35364dd2494f72e42fde4ce173a738bc8d
wlctatalc <wlctatalc@qq.com>
2021-03-11 17:40:29 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,6 @@
 
 # Add a feed source
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/V2RaySSR/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/V2RaySSR/small' feeds.conf.default
